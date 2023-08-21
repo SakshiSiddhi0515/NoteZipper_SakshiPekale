986f1c36958c048e972a70249103202583d25316
Sakshi Sujit Pekale <114364864+SakshiSiddhi0515@users.noreply.github.com>
2023-08-21 16:51:04 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return(
+  <div>
+    <div className="App">Hello World</div>
+    <div className="App">Hello World2</div>
+  </div>
+  
+  );
+      
+}
+
+export default App;
